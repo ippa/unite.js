028a840691994438e4b712ef2bc079a95966d1a5
ippa <iplord@iplord.com>
2013-12-07 15:17:17 +0100

    remove polyfills from core

--- src/core.js
+++ src/core.js
@@ -355,6 +355,12 @@ var unite = (function(unite) {
     var new_element, prev_element;
 
     // console.log(">> loopElement() .. found ", reusable_elements.length, " reusable elements");
+    /*
+    console.log(">> loopElement() element/scopes/binding");
+    console.log(element)
+    console.log(scopes)
+    console.log(binding)
+    */
 
     //
     // TEMP HACK UNTILL WE SORT OUT:
@@ -389,7 +395,6 @@ var unite = (function(unite) {
         applyElement(new_element, scope, binding);
         if(prev_element)  insertAfter(prev_element, new_element);
         else              insertAfter(element, new_element);
-
       }
       prev_element = new_element;
     }
@@ -509,7 +514,7 @@ var unite = (function(unite) {
    */
   function getValue(variable) {
     if(!variable) return undefined;
-    // console.log("* getValue(" + variable +")")
+    console.log("* getValue(" + variable +")")
     var tmp, object, prev;
     
     /* ".." means breakout-value, basically <div scope="app">{{.value}}</div> */
@@ -614,60 +619,6 @@ var unite = (function(unite) {
   function genID() {
     return Math.floor(Math.random() * 0x100000000).toString(16) + Math.floor(Math.random() * 0x100000000).toString(16);
   }
-
-  /* 
-   * START POLYFILLS
-   */
-  if(!Array.prototype.forEach) {
-    Array.prototype.forEach = function (fn, scope) {
-      var i, len;
-      for (i = 0, len = this.length; i < len; ++i) {
-        if(i in this) {
-          fn.call(scope, this[i], i, this);
-        }
-      }
-    };
-  }
-
-  if(!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(needle) {
-      var index = -1;
-
-      for(var i=0; i < this.length; i++) {
-        if(this[i] === needle) {
-          index = i;
-          break;
-        }
-      }
-      return index;
-    };
-  }
-
-  /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind */
-  if (!Function.prototype.bind) {
-    Function.prototype.bind = function (oThis) {
-      if (typeof this !== "function") {
-        // closest thing possible to the ECMAScript 5 internal IsCallable function
-        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-      }
-
-      var aArgs = Array.prototype.slice.call(arguments, 1), 
-          fToBind = this, 
-          fNOP = function () {},
-          fBound = function () {
-            return fToBind.apply(this instanceof fNOP && oThis
-                ? this
-                : oThis,
-                aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
-
-      fNOP.prototype = this.prototype;
-      fBound.prototype = new fNOP();
-
-      return fBound;
-    };
-  }
-  /* END POLYFILLS */
 
   return unite;
 })(unite || {});
